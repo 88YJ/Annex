d3f239f0e240db85ac707f0af00b0282d54c652a
brandon085 <brandon.frybailey@gmail.com>
2020-07-09 19:50:19 -0700

    created store redirect

--- client/src/components/pages/Cart.js
+++ client/src/components/pages/Cart.js
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect } from "react";
-
-import AuthContext from "../../context/auth/authContext";
-import StoreContext from "../../context/store/storeContext";
-import ServerContext from "../../context/server/serverContext";
+import React, { useContext, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import StoreContext from '../../context/store/storeContext';
+import ServerContext from '../../context/server/serverContext';
 
 const StoreGamePage = () => {
  const authContext = useContext(AuthContext);
@@ -27,32 +27,35 @@ const StoreGamePage = () => {
   let game = gamescart[0];
   buyGame(game);
  }
-
- return (
-  <div className='cartpage'>
-   <ul>
-    {gamescart.map((cart, i) => (
-     <li key={i}>
-      <h3 className='center'>{cart.name}</h3>
-      <div
-       className='cartimg'
-       style={{
-        backgroundImage: `url(${cart.wideimg})`,
-       }}
-      ></div>
-      <h3>Price: $60.00</h3>
-      <button className='globalbutton'>Remove From Cart</button>
-     </li>
-    ))}
-   </ul>
-   <div>
-    <button onClick={buy} className='globalbutton'>
-     Buy
-    </button>
-    <h3>Total: $60.00</h3>
+ if (gamescart.length > 0) {
+  return (
+   <div className='cartpage'>
+    <ul>
+     {gamescart.map((cart, i) => (
+      <li key={i}>
+       <h3 className='center'>{cart.name}</h3>
+       <div
+        className='cartimg'
+        style={{
+         backgroundImage: `url(${cart.wideimg})`,
+        }}
+       ></div>
+       <h3>Price: $60.00</h3>
+       <button className='globalbutton'>Remove From Cart</button>
+      </li>
+     ))}
+    </ul>
+    <div>
+     <button onClick={buy} className='globalbutton'>
+      Buy
+     </button>
+     <h3>Total: $60.00</h3>
+    </div>
    </div>
-  </div>
- );
+  );
+ } else {
+  return <Redirect to='/store' />;
+ }
 };
 
 export default StoreGamePage;
